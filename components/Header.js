c627c09e1b24997338e71aca50f838eca46ba3c2
olimarie21 <olivia.underdah@gmail.com>
2022-07-26 15:19:27 -0700

    Creates base header layout

--- components/Header.js
+++ components/Header.js
@@ -7,14 +7,53 @@ const Header = () => {
 	return (
 		<HeaderContainer>
 			<Image
-				width={'100%'}
-				height={'100%'}
-				src={'/images/courte-logo.svg'}
-				alt={'Courte Logo'}></Image>
+				width={128}
+				height={24}
+				src={'/images/courte_logo.svg'}
+				alt={'Courte Logo'}
+			/>
+
+			{/* <Image
+				width={30}
+				height={30}
+				src={'/images/hamburger-menu.svg'}
+				alt='Menu icon'
+			/> */}
+
+			<DesktopMenu>
+				<MenuItem>Features</MenuItem>
+				<MenuItem>About</MenuItem>
+				<MenuItem>Team</MenuItem>
+				<MenuItem>Contact</MenuItem>
+			</DesktopMenu>
 		</HeaderContainer>
 	)
 }
 
 const HeaderContainer = styled.div`
 	display: flex;
+	max-height: 72px;
+	padding: 24px;
+	align-items: center;
+	justify-content: space-between;
+	position: sticky;
+	top: 0;
+	left: 0;
+	background: ${globalColor.grey0};
+	z-index: 4;
 `
+
+const DesktopMenu = styled.ul`
+	display: flex;
+	list-style: none;
+	margin: 0;
+	padding: 0;
+	gap: 40px;
+	font-size: 18px;
+	font-weight: 600;
+`
+const MenuItem = styled.li`
+	margin: 0;
+	padding: 0;
+`
+export default Header
