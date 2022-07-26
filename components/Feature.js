755a0fa97ab69b08cf4a78df5c76987f3667c3d5
olimarie21 <olivia.underdah@gmail.com>
2022-07-26 14:17:32 -0700

    Applies desktop styles to main feature

--- components/Feature.js
+++ components/Feature.js
@@ -18,12 +18,30 @@ export const ImageContainer = styled.div`
 	width: 342px;
 	height: 314px;
 	border-radius: 6px;
+	position: relative;
+	@media (min-width: 800px) {
+		width: 430px;
+		height: 395px;
+	}
 `
 
 export const FeatureContainer = styled.div`
 	display: flex;
 	flex-direction: column;
 	margin: 120px 24px;
+	@media (min-width: 800px) {
+		flex-direction: row;
+	}
+`
+
+const InfoContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	@media (min-width: 800px) {
+		margin-left: 42px;
+	}
+	max-width: 430px;
+	max-height: 395px;
 `
 
 const Feature = (props) => {
@@ -37,11 +55,15 @@ const Feature = (props) => {
 					alt='feature'
 					width={'342px'}
 					height={'314px'}
+					layout='fill'
+					quality={100}
 				/>
 			</ImageContainer>
-			<SectionTitle>{featureTitle}</SectionTitle>
-			<HeadLine>{featureHeadline}</HeadLine>
-			<DescriptiveText>{featureDescription}</DescriptiveText>
+			<InfoContainer>
+				<SectionTitle>{featureTitle}</SectionTitle>
+				<HeadLine>{featureHeadline}</HeadLine>
+				<DescriptiveText>{featureDescription}</DescriptiveText>
+			</InfoContainer>
 		</FeatureContainer>
 	)
 }
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,35 @@
 import Head from 'next/head'
 import Feature from '../components/Feature'
 import styles from '../styles/Home.module.css'
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export default function Home() {
-  return (
-    <Container>
-      <Head>
-        <title>Courte</title>
-        <meta name="description" content="Discover a better way to play tennis" />
-        <link rel="icon" href="/CourteIcon.ico" />
-      </Head>
+	return (
+		<Container>
+			<Head>
+				<title>Courte</title>
+				<meta
+					name='description'
+					content='Discover a better way to play tennis'
+				/>
+				<link rel='icon' href='/CourteIcon.ico' />
+			</Head>
 
-
-      <Test>
-        <p>Courte</p>
-      </Test>
-
-    </Container>
-  )
+			<Test>
+				<p>Courte</p>
+			</Test>
+		</Container>
+	)
 }
 const Container = styled.div`
-  padding: 0 6.2vw;
-  width: 100%;
-  height: 100vh;
+	padding: 0 6.2vw;
+	width: 100%;
+	height: 100vh;
 `
 const Test = styled.main`
-  font-weight: 700;
-  
-  p {
-    margin: 0
-  }
+	font-weight: 700;
+
+	p {
+		margin: 0;
+	}
 `
