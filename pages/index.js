452e672f09e042aebe515e6e6fac1583930519da
Gavriil84 <gabwt.84@gmail.com>
2022-07-26 18:10:20 -0700

    Set up components to render

--- pages/index.js
+++ pages/index.js
@@ -2,29 +2,36 @@ import Head from 'next/head'
 import Feature from '../components/Feature'
 import styles from '../styles/Home.module.css'
 import styled from 'styled-components'
+import Hero from '../components/Hero'
+import About from '../components/About'
+import Footer from '../components/Footer'
 
 export default function Home() {
-	return (
-		<Container>
-			<Head>
-				<title>Courte</title>
-				<meta
-					name='description'
-					content='Discover a better way to play tennis'
-				/>
-				<link rel='icon' href='/CourteIcon.ico' />
-			</Head>
+  return (
+    <div>
+      <Container>
+        <Head>
+          <title>Courte</title>
+          <meta
+            name='description'
+            content='Discover a better way to play tennis'
+          />
+          <link rel='icon' href='/CourteIcon.ico' />
+        </Head>
 
-			<Test>
-				<p>Courte</p>
-			</Test>
-		</Container>
-	)
+
+        <Hero />
+        <About />
+
+      </Container>
+      <Footer />
+    </div>
+  )
 }
 const Container = styled.div`
-	padding: 0 6.2vw;
-	width: 100%;
-	height: 100vh;
+    padding: 0 6.2vw;
+    width: 100vw;
+    min-height: 100vh;
 `
 const Test = styled.main`
 	font-weight: 700;
