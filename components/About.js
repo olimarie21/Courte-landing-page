7b9a86c03098a699efc693e9d0210c62d6f8e24a
Gavriil84 <gabwt.84@gmail.com>
2022-07-26 18:09:49 -0700

    Creates about component

---
+++ components/About.js
@@ -0,0 +1,100 @@
+import React from 'react'
+import styled from 'styled-components'
+import { DescriptiveText, globalColor, HeadLine, SectionTitle } from '../shared/Style'
+import about from '../public/images/image-about.png'
+import Image from 'next/image'
+
+const About = () => {
+    return (
+        <AboutWrapper>
+            <div className="imageWrapper">
+                <Image className="aboutImage" src={about} alt="Courte description" />
+            </div>
+
+            <div className="textWrapper">
+                <SectionTitle className="about">About</SectionTitle>
+                <HeadLine className="headLine">A solution tailored for public tennis courts.</HeadLine>
+                <DescriptiveText className="descriptiveText">
+                    In-person arrangements to share public tennis courts is not practical. Courte is here to help make using and sharing the court as easy as the press of a button.
+                </DescriptiveText>
+            </div>
+
+            <div className="buttonWrapper">
+                <a href="#about">
+                    <button className="button">
+                        <h3>View proposal</h3>
+                    </button>
+                </a>
+            </div>
+
+        </AboutWrapper>
+    )
+}
+
+const AboutWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background-color: ${globalColor.grey20};
+    height: 100%;
+    border-radius: 8px;
+    padding-right: 6.2vw;
+    padding-left: 6.2vw;
+    padding-bottom: 4.8vh;
+    margin-top: 13.7vh;
+
+    .imageWrapper{
+        margin-top: 3.7vh;
+        margin-left: auto;
+        margin-right: auto;
+        width: 100%;
+        display: flex;
+        flex-direction: center;
+        width: 55%;
+        .aboutImage {
+            width: 100%;
+            height: auto;
+
+        }
+    }
+    
+    .textWrapper{
+        margin-top: 6.2vh;
+        .headLine{
+            margin-top: 1.9vh;
+            margin-bottom:0;
+        }
+        .about{
+            margin: 0;
+        }
+        .descriptiveText{
+            margin-top: 1.9vh;
+        }
+    }
+
+    .buttonWrapper{
+        margin-top: 4.9vh;
+        .button{
+            cursor: pointer;
+            width: 194px;
+            height: 43px;
+            background-color: ${globalColor.primary};
+            border: none;
+            border-radius: 8px;
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+            padding: 10px 16px;
+            gap: 10px;
+            
+            h3{
+                color: ${globalColor.grey0};
+                font-weight:600;
+                font-family: 'outfit', sans-serif;
+            }
+        }
+    }
+    `
+
+export default About
