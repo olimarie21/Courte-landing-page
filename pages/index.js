bc01803f351bbf4db9aad1b93a88bad2109cd62d
olimarie21 <olivia.underdah@gmail.com>
2022-08-01 16:19:44 -0700

    Updates index with feature container

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import Head from 'next/head'
-import Feature from '../components/Feature'
-import styles from '../styles/Home.module.css'
 import styled from 'styled-components'
 import Header from '../components/Header'
+import FeatureContainer from '../components/FeatureContainer'
 
 export default function Home() {
 	return (
@@ -15,36 +14,16 @@ export default function Home() {
 				/>
 				<link rel='icon' href='/CourteIcon.ico' />
 			</Head>
-			<Header />
 
-			<Test>
-				<Feature
-					featureTitle='Explore'
-					featureHeadline='Find tennis courts and check their availability'
-					featureDescription='Save time by planning to visit parks with the shortest wait time and have easy access to all available court options in your community.'
-					featureImg='/images/image-explore.png'
-				/>
-				<Feature
-					featureTitle='Play'
-					featureHeadline='Join or queue at a court and track your play time'
-					featureDescription='Simplify the use and sharing of courts by organizing players in a queue system—and don’t lose sight of your place in line with notifications!'
-					featureImg='/images/image-play.png'
-				/>
-				<Feature
-					featureTitle='Match'
-					featureHeadline='Set your preferences and pair up with a player'
-					featureDescription='Cut down on communication and have a glance at other users’ looking for someone to play based on your preferences.'
-					featureImg='/images/image-match.png'
-				/>
-			</Test>
+			<Header />
+			<FeatureContainer />
 		</Container>
 	)
 }
 
 const Container = styled.div`
 	padding: 0;
-	width: 100%;
-	height: 100vh;
+	max-width: 900px;
 `
 const Test = styled.main`
 	font-weight: 700;
